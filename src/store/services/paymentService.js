f35ae2eb06b753661639880721c24289c291bb2b
kanwar-sikandar-ali <kanwarsikandarali@gmail.com>
2023-06-25 11:43:03 +0500

    first commit

---
+++ src/store/services/paymentService.js
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const paymentService = createApi({
+  reducerPath: "payment",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://192.168.23.134:32001/api/",
+    prepareHeaders: (headers, { getState }) => {
+      const reducers = getState();
+      const token = reducers?.authReducer?.userToken;
+      headers.set("authorization", token ? `Bearer ${token}` : "");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => {
+    return {
+      sendPayment: builder.mutation({
+        query: (cart) => {
+          return {
+            url: "/create-checkout-session",
+            method: "POST",
+            body: cart,
+          };
+        },
+      }),
+      webhookPayment: builder.mutation({
+        query: (cart) => {
+          return {
+            url: "/webhook",
+            method: "POST",
+            body: cart,
+          };
+        },
+      }),
+      verifyPayment: builder.query({
+        query: (id) => {
+          return {
+            url: `verify-payment/${id}`,
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+export const { useSendPaymentMutation,webhookPayment, useVerifyPaymentQuery } = paymentService;
+export default paymentService;
