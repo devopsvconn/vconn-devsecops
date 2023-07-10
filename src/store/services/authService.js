690f8002219282cbaa77f8006f4cfcab8ea9ef92
kanwarsaadali <117541488+kanwarsaadali@users.noreply.github.com>
2023-07-11 01:55:01 +0500

    Update authService.js

--- src/store/services/authService.js
+++ src/store/services/authService.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 const authService = createApi({
     reducerPath: 'auth',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://a8c0fed7f13334e3c98534becdae912f-2084228010.ap-south-1.elb.amazonaws.com/api/'
+        baseUrl: 'http://localhost:8000/api/'
     }),
     endpoints: (builder) => {
        return {
