acb1af99fa40bd2f6ea22ff5a41ef9fa03275475
kanwarsaadali <117541488+kanwarsaadali@users.noreply.github.com>
2023-08-01 03:58:06 +0500

    Update orderService.js

--- src/store/services/orderService.js
+++ src/store/services/orderService.js
@@ -4,7 +4,7 @@ const orderService = createApi({
   reducerPath: "orders",
   tagTypes: "orders",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://saad-env.eba-b8ntatks.ap-south-1.elasticbeanstalk.com/api/",
+    baseUrl: "http://52.66.197.179:32001/api/",
     prepareHeaders: (headers, { getState }) => {
       const reducers = getState();
       const token = reducers?.authReducer?.adminToken;
