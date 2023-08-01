92d616c184e885d3f31b8150a5acbcf59834e175
kanwarsaadali <117541488+kanwarsaadali@users.noreply.github.com>
2023-08-01 14:26:18 +0500

    Update homeProducts.js

--- src/store/services/homeProducts.js
+++ src/store/services/homeProducts.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const homeProducts = createApi({
   reducerPath: "homeProducts",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://52.66.197.179:32001/",
+    baseUrl: "http://52.66.197.179:32001/api/",
   }),
   endpoints: (builder) => {
     return {
