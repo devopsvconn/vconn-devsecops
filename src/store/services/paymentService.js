9b60e3ae322b2ea6067344f422c5479fc2101932
kanwarsaadali <117541488+kanwarsaadali@users.noreply.github.com>
2023-08-01 14:32:45 +0500

    Update paymentService.js

--- src/store/services/paymentService.js
+++ src/store/services/paymentService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const paymentService = createApi({
   reducerPath: "payment",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://52.66.197.179:32001/api/",
+    baseUrl: "http://a1188382a7467456694b3b4427a216b1-700100809.ap-south-1.elb.amazonaws.com/api/",
     prepareHeaders: (headers, { getState }) => {
       const reducers = getState();
       const token = reducers?.authReducer?.userToken;
