aa2b5bd71decbb869311aa6cb705829cc144f19c
kanwarsaadali <117541488+kanwarsaadali@users.noreply.github.com>
2023-08-01 14:20:10 +0500

    Update productService.js

--- src/store/services/productService.js
+++ src/store/services/productService.js
@@ -4,7 +4,7 @@ const productService = createApi({
     reducerPath: 'products',
     tagTypes: 'products',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http/52.66.197.179:32001/api/',
+        baseUrl: 'http://52.66.197.179:32001/',
         prepareHeaders: (headers, {getState}) => {
             const reducers = getState();
             const token = reducers?.authReducer?.adminToken;
