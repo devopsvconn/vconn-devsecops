6b67d1728174d43a3f0191a9f1d48f401472588f
devopsvconn <155450292+devopsvconn@users.noreply.github.com>
2024-01-17 08:55:34 +0000

    Update paymentService.js

--- src/store/services/paymentService.js
+++ src/store/services/paymentService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const paymentService = createApi({
   reducerPath: "payment",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.1.113.248/api/",
+    baseUrl: "http://10.1.113.249/api/",
     prepareHeaders: (headers, { getState }) => {
       const reducers = getState();
       const token = reducers?.authReducer?.userToken;
