04ed2f40d4276b308e58cf0cd1344494f3387b17
devopsvconn <155450292+devopsvconn@users.noreply.github.com>
2024-01-17 08:56:29 +0000

    Update userOrdersService.js

--- src/store/services/userOrdersService.js
+++ src/store/services/userOrdersService.js
@@ -4,7 +4,7 @@ const userOrdersService = createApi({
   reducerPath: "user-orders",
   tagTypes: "orders",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.1.113.248/api/",
+    baseUrl: "http://10.1.113.249/api/",
     prepareHeaders: (headers, { getState }) => {
       const reducers = getState();
       const token = reducers?.authReducer?.userToken;
